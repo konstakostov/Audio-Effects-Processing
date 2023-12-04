b5340c33902a2b64a1df1d73fcf9044b0903f363
konstakostov <122868401+konstakostov@users.noreply.github.com>
2023-12-04 16:39:29 +0200

    Added additional effects information

--- GUI/Effects/effect_parameters.py
+++ GUI/Effects/effect_parameters.py
@@ -7,13 +7,38 @@ class Effects:
             ['Feedback', 0, 1],
             ['Mix', 0, 1],
         ],
+
         'Clipping': [
             ['Threshold [dB]', -100, 100],
         ],
-        'Distortion': [],
-        'Phaser': [],
-        'Compressor': [],
-        'Gain': [],
-        'Limiter': [],
+
+        'Distortion': [
+            ['Drive [dB]', 0, 100],
+        ],
+
+        'Phaser': [
+            ['Rate [Hz]', 0, 100],
+            ['Depth', 0, 1],
+            ['Center Frequency', 0, 1000],
+            ['Feedback', 0, 1],
+            ['Mix', 0, 1],
+        ],
+
+        'Compressor': [
+            ['Rate [Hz]', 0, 100],
+            ['Threshold [dB]', -100, 100],
+            ['Ratio', 0, 1],
+            ['Attack [ms]', 0, 10000],
+            ['Release [ms]', 0, 10000],
+        ],
+
+        'Gain': [
+            ['Gain [dB]', 0, 100],
+        ],
+
+        'Limiter': [
+            ['Threshold [dB]', -100, 100],
+            ['Release [ms]', 0, 10000],
+        ],
     }
 
