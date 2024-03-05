a15fe8ad6e14e92e85c342bc377acb024fd7e665
konstakostov <122868401+konstakostov@users.noreply.github.com>
2024-03-05 18:09:23 +0200

    Button to open the directory of the last chosen .wav file is defined and it's functionality has been added.

--- GUI/Window/Main_Window/main_window.py
+++ GUI/Window/Main_Window/main_window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from GUI.Widgets.Buttons.choose_wav_file_button import define_choose_wav_file_button
+from GUI.Widgets.Buttons.open_last_selected_directory_button import define_open_last_directory_button
 from GUI.Widgets.Buttons.process_wav_file_button import define_process_wav_file_button
 from GUI.Window.Main_Window.Main_Window_Frame.main_window_effects_frame_call import call_main_window_effects_frames
 from GUI.Window.Main_Window.Main_Window_Frame.main_window_effects_frame_input_data import MainWindowEffectsFrameInputData
@@ -13,6 +14,7 @@ The 'define_main_window_size' function is used to determine the size and propert
 The 'call_main_window_effects_frames' function is used to call all the Frames, displayed in the Main Window. 
 The 'define_choose_wav_file_button' function is used to call the Button for choosing .wav file to process.
 The 'define_process_wav_file_button' function is used to call the Button for processing the selected .wav file.
+The 'define_open_last_directory_button' function is used to open the last selected input .wav file directory.
 """
 
 
@@ -27,5 +29,7 @@ class MainWindow(tk.Tk):
         define_choose_wav_file_button(self)
 
         define_process_wav_file_button(self)
+
+        define_open_last_directory_button(self)
 
 
